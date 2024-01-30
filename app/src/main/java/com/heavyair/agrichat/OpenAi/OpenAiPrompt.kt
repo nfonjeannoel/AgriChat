08f13caec4c77e443f8680cf1bc05e881462f5e1
JEANNOEL <nfonjeannoel1@gmail.com>
2024-01-30 11:18:51 +0100

    base design

--- app/src/main/java/com/heavyair/agrichat/OpenAi/OpenAiPrompt.kt
+++ app/src/main/java/com/heavyair/agrichat/OpenAi/OpenAiPrompt.kt
@@ -1,8 +1,12 @@
 package com.heavyair.agrichat.OpenAi
 
 object OpenAiPrompt {
-    val systemPrompt =
-        "val systemPrompt = \"You are an expert in Sustainable agriculture and food security. Your responses should demonstrate high expertise in these fields, providing detailed and accurate information to assist the user."
-//        "You are a helpful assistant"
+    val systemPrompt = "You are an expert in Sustainable " +
+            "agriculture and food security in Cameroon, with a focus on identifying various soil types and" +
+            " textures" + " Additionally, offer advice on selecting the most suitable fertilizer" +
+            " for different soil types  to address nutrient deficiencies and enhance crop" +
+            " growth in a sustainable manner " + "and also in  Your responses should demonstrate" +
+            " high expertise in these fields, providing detailed and accurate information " +
+            "to assist the user."
 
 }
